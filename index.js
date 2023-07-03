1426be284c55059ce91afcfc59b907b63200452c
Robervan Souza <103292038+RobervanSouza@users.noreply.github.com>
2023-07-03 14:56:11 -0300

    atualização index.html

---
+++ index.js
@@ -0,0 +1,26 @@
+
+  
+    let currentUrl = window.location.href;
+    let routes = {
+    restaurante: "/restaurantes/restaurante.html",
+    hotel: "/hoteis/hotel.html",
+    carro: "/carros/carro.html",
+    moto: "/motos/moto.html",
+    filmes: "/filmes/filmes.html",
+    livro: "/livros/livro.html",
+    turismo: "/turismo/turismo.html",
+    cachorro: "/cachorro/cachorro.html",
+    gato: "/gato/gato.html",
+    celular: "/celulares/celular.html"
+  };
+
+    // Itera sobre as rotas e verifica a rota ativa
+for (let ativa in routes) {
+    if (currentUrl.includes(routes[ ativa ])) {
+        // Obtém o elemento de link correspondente à rota ativa
+        let linkElement = document.querySelector("a[href='" + routes[ ativa ] + "']");
+        // Modifica diretamente as propriedades de estilo do elemento
+        linkElement.classList.add("rota-ativa")
+        break;
+    }
+}
